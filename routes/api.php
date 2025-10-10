b629446c9499d92dd2814310517f31da0a05790a
shami2024 <>
2025-10-11 02:21:29 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -46,9 +46,6 @@ use App\Http\Controllers\API\PurchaseReturnAPIController;
 use App\Http\Controllers\API\ExpenseCategoryAPIController;
 use App\Http\Controllers\API\ProductCategoryAPIController;
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With');
 
 /*
 |--------------------------------------------------------------------------
