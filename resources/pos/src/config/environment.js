01f9aead3ed818d02c4f18e8ba387fbd29381ee3
shami2024 <>
2025-10-15 19:12:19 +0300

    Update environment.js

--- resources/pos/src/config/environment.js
+++ resources/pos/src/config/environment.js
@@ -1,5 +1,11 @@
+// export const environment = {
+//     URL: window.location.protocol + '//' + window.location.hostname,
+// };
+
 export const environment = {
-    URL: window.location.protocol + '//' + window.location.hostname,
+    API_URL: window.location.origin.includes('127.0.0.1') || window.location.origin.includes('localhost')
+        ? 'http://127.0.0.1:8000/'
+        : 'https://mabiea-pos.binary-tm.com/',
 };
 
 // export const environment = {
