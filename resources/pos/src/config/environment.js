6b869c2653a528c4c1d649c4dc47eda6e7e3ec7a
shami2024 <>
2025-10-17 14:05:25 +0300

    environment.js

--- resources/pos/src/config/environment.js
+++ resources/pos/src/config/environment.js
@@ -1,7 +1,10 @@
-export const environment = {
-    URL: window.location.protocol + '//' + window.location.hostname,
-};
+// export const environment = {
+//     URL: window.location.protocol + '//' + window.location.hostname,
+// };
 
+export const environment = {
+    URL: window.location.origin,
+};
 
 // export const environment = {
 //     URL: 'http://127.0.0.1:8000',
